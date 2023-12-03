511d1e6c1297383a1b5f7b05207d226fb6e17530
Rishabh Nahar <rishabhnahar1202@gmail.com>
2023-12-03 17:32:12 +0530

    changes

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let minItem = document.querySelector("#min");
 let secItem = document.querySelector("#sec");
 document.getElementById("myVideo").play();
 let countDown = () => {
-  let futureDate = new Date("9 Feb 2023");
+  let futureDate = new Date("9 Feb 2024");
   let currentDate = new Date();
   let myDate = futureDate - currentDate;
   //console.log(myDate);
